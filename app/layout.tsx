e804a320a759381abdbb30fc8d1017021dd41a6d
Eren Yalçın <yalcineren97@gmail.com>
2025-03-31 15:40:48 +0200

    move global css

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono as GeistMono } from "next/font/google";
-import "./globals.css";
+import "./styles/globals.css";
 import React from "react";
 
 const geistSans = Geist({
