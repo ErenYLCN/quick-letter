5a79ce17ff54c113667032eb7894422a17d64b83
Eren Yalçın <yalcineren97@gmail.com>
2025-04-08 20:02:08 +0200

    setup cookies

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,37 @@
+import { cookies } from "next/headers";
 import Button from "./components/button/Button";
 import Textarea from "./components/textarea/Textarea";
+import Form from "next/form";
 
-export default function Home() {
+export default async function Home() {
+  const cookie = await cookies();
+  const paragraphs = cookie.get("paragraphs");
+  const parsedParagraphs: string[] = paragraphs ? JSON.parse(paragraphs.value) : [];
+
+  const paragraphFormAction = async (formData: FormData) => {
+    "use server";
+    const cookie = await cookies();
+    const paragraph = formData.get("paragraph");
+
+    cookie.set("paragraphs", JSON.stringify([...JSON.parse((cookie.get("paragraphs")?.value || "[]") as string), paragraph]));
+  };
+
   return (
     <div>
-      <div>
-        <Textarea className={"w-full max-w-4/5"} placeholder={"write something..."} />
-      </div>
-      <div>
-        <Button>{"Saave Paragraph"}</Button>
-      </div>
+      <Form action={paragraphFormAction}>
+        <div>
+          <Textarea name={"paragraph"} className={"w-full max-w-4/5"} placeholder={"write something..."} />
+        </div>
+        <div>
+          <Button type={"submit"}>{"Saave Paragraph"}</Button>
+        </div>
+      </Form>
+
+      <ul>
+        {parsedParagraphs.map((p, i) => (
+          <li key={i}>{p}</li>
+        ))}
+      </ul>
     </div>
   );
 }
