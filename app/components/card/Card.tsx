510b5309ee65ec842c8a81717581686e4bcb755d
Eren Yalçın <yalcineren97@gmail.com>
2025-04-16 23:49:02 +0200

    setup loading page

--- app/components/card/Card.tsx
+++ app/components/card/Card.tsx
@@ -13,10 +13,10 @@ interface CardProps {
 
 function Card({ children, className = "", actions }: CardProps) {
   return (
-    <div className={`bg-gray-800 rounded-lg shadow-md p-6 ${className}`}>
+    <div className={`bg-gray-800 rounded-lg shadow-md p-6 flex items-center justify-between ${className}`}>
       {children}
       {actions?.map((action, index) => (
-        <button key={index} onClick={action.handler} className={"mt-4 bg-blue-500 text-white py-2 px-4 rounded"}>
+        <button key={index} onClick={action.handler} className={"bg-blue-500 text-white py-2 px-4 rounded"}>
           {action.label}
         </button>
       ))}
