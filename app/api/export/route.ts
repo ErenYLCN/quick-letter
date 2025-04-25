391ffeabe030508a37ee39a45620db9809793bf8
Eren Yalçın <yalcineren97@gmail.com>
2025-04-25 19:51:15 +0200

    test

--- app/api/export/route.ts
+++ app/api/export/route.ts
@@ -1,14 +1,9 @@
 import { NextResponse } from "next/server";
-import PDFDocument from "pdfkit";
 import fs from "fs";
 
 export async function POST(_request: Request) {
   try {
     console.log(fs);
-
-    const doc = new PDFDocument();
-
-    console.log(doc);
   } catch (error) {
     console.error("Error generating PDF:", error);
     return NextResponse.json({ error: "Failed to generate PDF" }, { status: 500 });
